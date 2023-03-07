3abd259deebec68a7f802ee02a4025e7ca43a954
riceexe <84036239+riceexe@users.noreply.github.com>
2023-03-07 11:29:55 -0500

    Update latite_installer.bat

--- latite_installer.bat
+++ latite_installer.bat
@@ -1,5 +1,4 @@
 @echo off
-del "%temp%\installer.bat"
 echo Oh no! The installer has stopped working. Please report this to the dev.
 echo Oh wait! The developer can't talk. Please yell at Xyrelium#6319 to fix this.
 pause

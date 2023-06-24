0110556468f6551585fed27d20a437e5dad00eee
rice <84036239+riceexe@users.noreply.github.com>
2023-06-24 16:33:35 -0400

    Update latite_installer.bat
    1.20.1

--- latite_installer.bat
+++ latite_installer.bat
@@ -2,7 +2,7 @@
 @:: by rice#2532 and VastraKai#0001
 @if not "%debug%" == "true" @echo off 
 :: The version of Latite to install.
-set LatiteVersion=1.20
+set LatiteVersion=1.20.1
 :: The directory to use for installation.
 set LatiteDir=%userprofile%\Latite
 :: The directory to install the appx to.
